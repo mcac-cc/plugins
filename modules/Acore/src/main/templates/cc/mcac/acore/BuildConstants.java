9f3bce9357f939fbb94c144b64e65835d707febf
Zhou Zihang <z@mcac.cc>
2026-02-26 22:47:21 +0800

    build: migrate Acore to Maven and unify CI

--- modules/Acore/src/main/templates/cc/mcac/acore/BuildConstants.java
+++ modules/Acore/src/main/templates/cc/mcac/acore/BuildConstants.java
@@ -3,5 +3,5 @@ package cc.mcac.acore;
 // The constants are replaced before compilation
 public class BuildConstants {
 
-    public static final String VERSION = "${version}";
+    public static final String VERSION = "${project.version}";
 }
