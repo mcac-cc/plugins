cf73711144d1a14e8db28e68a0e92b74a148cf7d
Zhou Zihang <z@mcac.cc>
2026-03-13 01:52:16 +0800

    fix(itemmanager): harden command handling

--- modules/ItemManager/src/main/java/com/mcatk/itemmanager/ItemManagerCommand.java
+++ modules/ItemManager/src/main/java/com/mcatk/itemmanager/ItemManagerCommand.java
@@ -4,11 +4,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class ItemManagerCommand implements CommandExecutor {
     private CommandSender sender;
     private String[] args;
-    
+
     void printHelp() {
         sender.sendMessage("帮助：严格按照格式执行");
         sender.sendMessage("创建类型: /im create <类型ID>");
@@ -16,7 +17,7 @@ public class ItemManagerCommand implements CommandExecutor {
         sender.sendMessage("获取物品: /im get <类型ID> <商品ID>");
         sender.sendMessage("列出物品: /im list");
     }
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.isOp()) {
@@ -30,47 +31,82 @@ public class ItemManagerCommand implements CommandExecutor {
         }
         switch (args[0].toLowerCase()) {
             case "create":
-                create();
-                break;
+                return create();
             case "add":
-                add();
-                break;
+                return add();
+            case "get":
             case "give":
-                give();
-                break;
+                return give();
             case "list":
                 list();
-                break;
+                return true;
             default:
+                printHelp();
+                return true;
         }
-        return true;
     }
-    
-    private void create() {
+
+    private boolean create() {
+        if (args.length < 2) {
+            sender.sendMessage("用法: /im create <类型ID>");
+            return true;
+        }
+
         String sortId = args[1];
         ItemManager.getItemSort().createSort(sortId);
         sender.sendMessage("Ok");
+        return true;
     }
-    
-    private void add() {
+
+    private boolean add() {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("该命令只能由玩家执行。");
+            return true;
+        }
+        if (args.length < 3) {
+            sender.sendMessage("用法: /im add <类型ID> <商品ID>");
+            return true;
+        }
+
         String sortId = args[1];
         String itemId = args[2];
-        ItemManager.getItemSort().addItem(sortId, itemId,
-                ((Player) sender).getInventory().getItemInMainHand());
+        Player player = (Player) sender;
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        if (itemInHand == null || itemInHand.getType().isAir()) {
+            sender.sendMessage("请先手持要保存的物品。");
+            return true;
+        }
+
+        ItemManager.getItemSort().addItem(sortId, itemId, itemInHand);
         sender.sendMessage("Ok");
+        return true;
     }
-    
-    private void give() {
+
+    private boolean give() {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("该命令只能由玩家执行。");
+            return true;
+        }
+        if (args.length < 3) {
+            sender.sendMessage("用法: /im get <类型ID> <商品ID>");
+            return true;
+        }
+
         String sortId = args[1];
         String itemId = args[2];
-        ((Player) sender).getInventory().addItem(
-                ItemManager.getItemSort().getItem(sortId, itemId)
-        );
+        ItemStack item = ItemManager.getItemSort().getItem(sortId, itemId);
+        if (item == null) {
+            sender.sendMessage("未找到对应物品: " + sortId + "/" + itemId);
+            return true;
+        }
+
+        ((Player) sender).getInventory().addItem(item.clone());
         sender.sendMessage("Ok");
+        return true;
     }
-    
+
     private void list() {
         sender.sendMessage(ItemManager.getItemSort().listAll());
     }
-    
+
 }
--- modules/ItemManager/src/main/java/com/mcatk/itemmanager/ItemSort.java
+++ modules/ItemManager/src/main/java/com/mcatk/itemmanager/ItemSort.java
@@ -1,13 +1,14 @@
 package com.mcatk.itemmanager;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemSort {
     private final HashMap<String, Items> itemsHashMap;
-    
+
     public ItemSort() {
         itemsHashMap = new HashMap<>();
         StringBuilder stringBuilder = new StringBuilder("物品类已载入:\n");
@@ -17,11 +18,11 @@ public class ItemSort {
         }
         ItemManager.getPlugin().getLogger().info(stringBuilder.toString());
     }
-    
+
     public void createSort(String id) {
         itemsHashMap.put(id, new Items(id));
     }
-    
+
     public void addItem(String id1, String id2, ItemStack itemStack) {
         if (!itemsHashMap.containsKey(id1)) {
             itemsHashMap.put(id1, new Items(id1));
@@ -30,23 +31,38 @@ public class ItemSort {
         ItemManager.getPlugin().getConfig().set(id1 + "." + id2, itemStack);
         ItemManager.getPlugin().saveConfig();
     }
-    
+
     public ItemStack getItem(String id1, String id2) {
-        return itemsHashMap.get(id1).getItemStackHashMap().get(id2);
+        Items items = itemsHashMap.get(id1);
+        if (items == null) {
+            return null;
+        }
+        return items.getItemStackHashMap().get(id2);
     }
-    
+
     public String listAll() {
         StringBuilder stringBuilder = new StringBuilder("ItemManager-List:\n");
         for (Items items : itemsHashMap.values()) {
             stringBuilder.append("§2类型(").append(items.getId()).append("):\n");
             for (Map.Entry<String, ItemStack> entry :
                     items.getItemStackHashMap().entrySet()) {
-                stringBuilder.append(entry.getValue().getItemMeta().getDisplayName())
+                stringBuilder.append(getItemDisplayName(entry.getValue()))
                         .append("§f(").append(entry.getKey()).append(") ");
             }
             stringBuilder.append("\n");
         }
         return stringBuilder.toString();
     }
-    
+
+    private String getItemDisplayName(ItemStack itemStack) {
+        if (itemStack == null) {
+            return "未知物品";
+        }
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta != null && itemMeta.hasDisplayName()) {
+            return itemMeta.getDisplayName();
+        }
+        return itemStack.getType().name();
+    }
+
 }
